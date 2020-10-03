51e347a9aeed6bdb9a46299f33653e460cb6d6b3
jelani simeon <jelanisimeon@jelanis-MacBook-Pro.local>
2020-10-02 21:32:12 -0400

    first commit

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by jelani Simeon on 9/29/20.
+//  Copyright © 2020 jsimeon. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+}
